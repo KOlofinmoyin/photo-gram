65012d2fad190aa076b299e71da801366a87f0a1
Kehinde Olofinmoyin <kehindeolofinmoyin@hotmail.com>
2021-01-22 08:26:57 +0000

    corrected typo.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   has_one_attached :image #, styles: { :medium => "640x" }
-  validates :images, presence: true, blob: { content_type: ['image/jpg', 'image/jpeg', 'image/png'], size_range: 1..3.megabytes }
+  validates :image, presence: true, blob: { content_type: ['image/jpg', 'image/jpeg', 'image/png'], size_range: 1..3.megabytes }
   validates :caption, presence: true
 end
